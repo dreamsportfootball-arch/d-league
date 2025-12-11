4746268b126a027b16bc635f69e55447dd4c2f12
dreamsportfootball <dreamsportfootball@gmail.com>
2025-12-11 14:05:04 +0800

    Updates

--- assets/AboutPage-d7555OrH.js
+++ assets/AboutPage-d7555OrH.js
@@ -1,4 +1,4 @@
-import{c as s,j as e,g as a}from"./index-CW-oD5Kw.js";/**
+import{c as s,j as e,g as a}from"./index-G0KZwaST.js";/**
  * @license lucide-react v0.554.0 - ISC
  *
  * This source code is licensed under the ISC license.
--- assets/StandingsPage-Cpi9zBJl.js
+++ assets/StandingsPage-Cpi9zBJl.js
@@ -1,4 +1,4 @@
-import{c as l,r as x,j as e,b as m,S as o}from"./index-CW-oD5Kw.js";/**
+import{c as l,r as x,j as e,b as m,S as o}from"./index-G0KZwaST.js";/**
  * @license lucide-react v0.554.0 - ISC
  *
  * This source code is licensed under the ISC license.
