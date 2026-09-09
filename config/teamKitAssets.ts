86f25e577c5153007478e6383c4d405d935ab84b
dreamsportfootball <wangchiken0912@gmail.com>
2026-09-09 14:15:27 +0800

    feat: connect newly uploaded 2026-27 team kits

--- config/teamKitAssets.ts
+++ config/teamKitAssets.ts
@@ -15,6 +15,30 @@ const TEAM_KIT_ASSETS: Partial<Record<SeasonId, Record<string, TeamKitAssetPair>
       home: 'assets/seasons/2026-27/kits/black-wolf-fc-home.png',
       away: 'assets/seasons/2026-27/kits/black-wolf-fc-away.png',
     },
+    t_alian_2026: {
+      home: 'assets/seasons/2026-27/kits/alian-fc-home.png',
+      away: 'assets/seasons/2026-27/kits/alian-fc-away.png',
+    },
+    t_kaohsiung_hk_2026: {
+      home: 'assets/seasons/2026-27/kits/kaohsiung-hongkongers-fc-home.png',
+      away: 'assets/seasons/2026-27/kits/kaohsiung-hongkongers-fc-away.png',
+    },
+    t_luzhu_2026: {
+      home: 'assets/seasons/2026-27/kits/luzhu-club-home.png',
+      away: 'assets/seasons/2026-27/kits/luzhu-club-away.png',
+    },
+    t_chen_2026: {
+      home: 'assets/seasons/2026-27/kits/nanzhou-chen-home.png',
+      away: 'assets/seasons/2026-27/kits/nanzhou-chen-away.png',
+    },
+    t_pingtung_wolves_2026: {
+      home: 'assets/seasons/2026-27/kits/pingtung-wolves-fc-home.png',
+      away: 'assets/seasons/2026-27/kits/pingtung-wolves-fc-away.png',
+    },
+    t_niaoshi_2026: {
+      home: 'assets/seasons/2026-27/kits/tainan-niaoshi-home.png',
+      away: 'assets/seasons/2026-27/kits/tainan-niaoshi-away.png',
+    },
   },
 };
 
